29f0b9e1c59b01d04def7cfdc098515e1876a661
Trustin Nguyen <trustin.nguyen62@berkeley.edu>
2024-10-13 16:27:07 -0700

    Added context

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./globals.css";
+import { TaskListProvider } from "./providers/TaskContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <TaskListProvider>
+      <App />
+    </TaskListProvider>
+    ,
   </React.StrictMode>,
 );
